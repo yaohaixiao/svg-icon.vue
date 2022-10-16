bebed858be5f23f7dead79b201cbf26b582e2f42
Yaohaixiao <yaohaixiao@gmail.com>
2022-10-16 10:37:08 +0800

    test: 1. 添加加载自定义图标集的单侧用例；

--- README.md
+++ README.md
@@ -139,13 +139,13 @@ SvgIcon 组件即可以使用包含 icon 的父节点设置 CSS 样式控制图�
  * Update: 2022.10.16
  */
 const icoMoonSet = {
-    title: 'icoMoon 图标集',
-    symbols: [
-      '<symbol id="icon-moon-home" viewBox="0 0 16 16"><path d="M16 9.226l-8-6.21-8 6.21v-2.532l8-6.21 8 6.21zM14 9v6h-4v-4h-4v4h-4v-6l6-4.5z"></path></symbol>',
-      '<symbol id="icon-moon-home2" viewBox="0 0 16 16"><path d="M8 0.5l-8 8 1.5 1.5 1.5-1.5v6.5h4v-3h2v3h4v-6.5l1.5 1.5 1.5-1.5-8-8zM8 7c-0.552 0-1-0.448-1-1s0.448-1 1-1c0.552 0 1 0.448 1 1s-0.448 1-1 1z"></path></symbol>',
-      '<symbol id="icon-moon-home3" viewBox="0 0 16 16"><path d="M16 9.5l-3-3v-4.5h-2v2.5l-3-3-8 8v0.5h2v5h5v-3h2v3h5v-5h2z"></path></symbol>'
-    ]
-  }
+  title: 'icoMoon 图标集',
+  symbols: [
+    '<symbol id="icon-moon-home" viewBox="0 0 16 16"><path d="M16 9.226l-8-6.21-8 6.21v-2.532l8-6.21 8 6.21zM14 9v6h-4v-4h-4v4h-4v-6l6-4.5z"></path></symbol>',
+    '<symbol id="icon-moon-home2" viewBox="0 0 16 16"><path d="M8 0.5l-8 8 1.5 1.5 1.5-1.5v6.5h4v-3h2v3h4v-6.5l1.5 1.5 1.5-1.5-8-8zM8 7c-0.552 0-1-0.448-1-1s0.448-1 1-1c0.552 0 1 0.448 1 1s-0.448 1-1 1z"></path></symbol>',
+    '<symbol id="icon-moon-home3" viewBox="0 0 16 16"><path d="M16 9.5l-3-3v-4.5h-2v2.5l-3-3-8 8v0.5h2v5h5v-3h2v3h5v-5h2z"></path></symbol>'
+  ]
+}
 
 export default icoMoonSet
 
--- tests/SvgIcon.spec.js
+++ tests/SvgIcon.spec.js
@@ -2,7 +2,13 @@ import {
   shallowMount
 } from '@vue/test-utils'
 
+import icoMoonSet from '../documentation/assets/ico-moon'
 import SvgIcon from '../src/SvgIcon'
+
+import icons from '@/assets/default'
+import { render } from '@/utils/utils'
+
+const ICON_HOME = 'moon-home'
 
 describe('SvgIcon.vue', () => {
   let wrapper
@@ -46,13 +52,16 @@ describe('SvgIcon.vue', () => {
 
   it('调整 props.name 值为：cancel', async () => {
     const CANCEL = 'cancel'
+
     wrapper.setProps({
       name: CANCEL
     })
     await wrapper.vm.$nextTick()
+
     const binds = {
       'xlink:href': `#icon-${wrapper.vm.name}`
     }
+
     expect(wrapper.vm.name).toEqual(CANCEL)
     expect(wrapper.vm.binds).toEqual(binds)
   })
@@ -60,14 +69,17 @@ describe('SvgIcon.vue', () => {
   it('调整 props.size 值为：28', async () => {
     const SIZE = 28
     const CANCEL = 'cancel'
+
     wrapper.setProps({
       size: SIZE,
       name: CANCEL
     })
     await wrapper.vm.$nextTick()
+
     const binds = {
       'xlink:href': `#icon-${wrapper.vm.name}`
     }
+
     expect(wrapper.vm.size).toEqual(SIZE)
     expect(wrapper.vm.binds).toEqual(binds)
   })
@@ -75,15 +87,42 @@ describe('SvgIcon.vue', () => {
   it('调整 props.color 值为：#f00', async () => {
     const COLOR = '#f00'
     const CANCEL = 'cancel'
+
     wrapper.setProps({
       color: COLOR,
       name: CANCEL
     })
     await wrapper.vm.$nextTick()
+
     const binds = {
       'xlink:href': `#icon-${wrapper.vm.name}`
     }
+
     expect(wrapper.vm.color).toEqual(COLOR)
     expect(wrapper.vm.binds).toEqual(binds)
+  })
+
+  it('render() 加载自定义图标集：不追加至内置图标库', async () => {
+    render(icoMoonSet)
+
+    const icon = icons.find((iconSet) => {
+      return iconSet.symbols.find((symbol) => {
+        return symbol.indexOf(`icon-${ICON_HOME}`) > -1
+      })
+    })
+
+    expect(icon).toBe(undefined)
+  })
+
+  it('render() 加载自定义图标集：追加至内置图标库', async () => {
+    render(icoMoonSet, true)
+
+    const icon = icons.find((iconSet) => {
+      return iconSet.symbols.find((symbol) => {
+        return symbol.indexOf(`icon-${ICON_HOME}`) > -1
+      })
+    })
+
+    expect(icon.title).toEqual('icoMoon 图标集')
   })
 })
