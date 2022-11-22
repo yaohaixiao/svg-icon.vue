57771ca488f24a84632344d9ee3ddcd13deabe25
Yaohaixiao <yaohaixiao@gmail.com>
2022-11-22 16:23:59 +0800

    fix: 修复发布 package 配置报错问题

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import SvgIcon from './SvgIcon'
-
-/* istanbul ignore next */
-SvgIcon.install = function (Vue) {
-  Vue.component(SvgIcon.name, SvgIcon)
-}
-
-export default SvgIcon
--- vue.config.js
+++ vue.config.js
@@ -9,6 +9,7 @@ const path = require('path')
 const BundleAnalyzerPlugin =
   require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 const PreloadWebpackPlugin = require('@vue/preload-webpack-plugin')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
 
 const pkg = require('./package.json')
 
@@ -62,82 +63,88 @@ module.exports = {
       config.plugin('bundle-analyzer').use(BundleAnalyzerPlugin).end()
     })
 
-    config.plugin('html').tap((args) => {
-      const description = `${pkg.description}`
-      const arg = args[0]
-
-      arg.title = `svg-icon.vue - v${pkg.version} | ${description}`
-      arg.keywords = `javascript,svg,icon,svg-icon.vue,vue,vue.js`
-      arg.description = description
-
-      return args
-    })
-
-    // it can improve the speed of the first screen, it is recommended to turn on preload
     config
-      .plugin('preload')
-      .use(PreloadWebpackPlugin)
-      .tap(() => [
-        {
-          rel: 'preload',
-          // to ignore runtime.js
-          // https://github.com/vuejs/vue-cli/blob/dev/packages/@vue/cli-service/lib/config/app.js#L171
-          fileBlacklist: [/\.map$/, /runtime\..*\.js$/],
-          // initial, asyncChunks, all, allAssets
-          include: 'initial'
-        }
-      ])
+      .plugin('html')
+      .use(HTMLWebpackPlugin)
+      .tap(() => {
+        const description = `${pkg.description}`
 
-    // prefetch all asyncChunks
-    config
-      .plugin('prefetch')
-      .use(PreloadWebpackPlugin)
-      .tap(() => [
-        {
-          rel: 'prefetch',
-          // fileBlacklist: [/(Api|Usage|Page|Module)(.*?)\.(js|css)$/],
-          include: {
-            type: 'asyncChunks',
-            entries: ['app']
+        return [
+          {
+            title: `svg-icon.vue - v${pkg.version} | ${description}`,
+            keywords: `javascript,svg,icon,svg-icon.vue,vue,vue.js`,
+            description: description
+          }
+        ]
+      })
+
+    config.when(process.env.BUILD_FOR === 'docs', (config) => {
+      // it can improve the speed of the first screen, it is recommended to turn on preload
+      config
+        .plugin('preload')
+        .use(PreloadWebpackPlugin)
+        .tap(() => [
+          {
+            rel: 'preload',
+            // to ignore runtime.js
+            // https://github.com/vuejs/vue-cli/blob/dev/packages/@vue/cli-service/lib/config/app.js#L171
+            fileBlacklist: [/\.map$/, /runtime\..*\.js$/],
+            // initial, asyncChunks, all, allAssets
+            include: 'initial'
+          }
+        ])
+
+      // prefetch all asyncChunks
+      config
+        .plugin('prefetch')
+        .use(PreloadWebpackPlugin)
+        .tap(() => [
+          {
+            rel: 'prefetch',
+            // fileBlacklist: [/(Api|Usage|Page|Module)(.*?)\.(js|css)$/],
+            include: {
+              type: 'asyncChunks',
+              entries: ['app']
+            }
+          }
+        ])
+
+      config.optimization.splitChunks({
+        chunks: 'all',
+        cacheGroups: {
+          vue: {
+            name: 'chunk-vuejs',
+            test: /[\\/]node_modules[\\/]_?vue(.*)/,
+            priority: 30,
+            chunks: 'initial',
+            reuseExistingChunk: true
+          },
+          libs: {
+            name: 'chunk-libs',
+            test: /[\\/]node_modules[\\/]/,
+            priority: 22,
+            reuseExistingChunk: true
+          },
+          icons: {
+            name: 'chunk-icons',
+            // the weight needs to be larger than libs and app, or it will be packaged into libs or app
+            priority: 18,
+            test: resolve('src/assets'),
+            reuseExistingChunk: true
+          },
+          commons: {
+            name: 'chunk-commons',
+            // the weight needs to be larger than libs and app, or it will be packaged into libs or app
+            priority: 16,
+            test: resolve('documentation/components'),
+            reuseExistingChunk: true
           }
         }
-      ])
+      })
 
-    config.optimization.splitChunks({
-      chunks: 'all',
-      cacheGroups: {
-        vue: {
-          name: 'chunk-vuejs',
-          test: /[\\/]node_modules[\\/]_?vue(.*)/,
-          priority: 30,
-          chunks: 'initial',
-          reuseExistingChunk: true
-        },
-        libs: {
-          name: 'chunk-libs',
-          test: /[\\/]node_modules[\\/]/,
-          priority: 22,
-          reuseExistingChunk: true
-        },
-        icons: {
-          name: 'chunk-icons',
-          // the weight needs to be larger than libs and app, or it will be packaged into libs or app
-          priority: 18,
-          test: resolve('src/assets'),
-          reuseExistingChunk: true
-        },
-        commons: {
-          name: 'chunk-commons',
-          // the weight needs to be larger than libs and app, or it will be packaged into libs or app
-          priority: 16,
-          test: resolve('documentation/components'),
-          reuseExistingChunk: true
-        }
-      }
+      // https://webpack.js.org/configuration/optimization/#optimizationruntimechunk
+      config.optimization.runtimeChunk('single')
     })
-
-    // https://webpack.js.org/configuration/optimization/#optimizationruntimechunk
-    config.optimization.runtimeChunk('single')
   },
 
   transpileDependencies: true
